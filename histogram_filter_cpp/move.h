acc56c7d273c660b93fb9245cb4537923632ee51
Sakshi Bhatia <40262132+sakshi79@users.noreply.github.com>
2023-04-18 16:28:27 +0530

    Delete histogram_filter_cpp directory

--- histogram_filter_cpp/move.h
+++
@@ -1,5 +0,0 @@
-#include <vector>
-#include <string>
-using namespace std;
-
-vector<float> move(vector<float> p, int U, float pExact, float pOvershoot, float pUndershoot);
--- histogram_filter_cpp/sense.h
+++
@@ -1,5 +0,0 @@
-#include <vector>
-#include <string>
-using namespace std;
-
-vector<float> sense(vector<float> p, string Z, vector<string> world, float pHit, float pMiss);
